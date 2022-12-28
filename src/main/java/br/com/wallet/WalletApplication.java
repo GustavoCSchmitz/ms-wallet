9f9aca478bcffa5e17ff4d7fd42030084835ddcd
Gustavo <gu_schmitz@hotmail.com>
2022-12-28 15:28:19 -0300

    Adding feign dependency

--- src/main/java/br/com/wallet/WalletApplication.java
+++ src/main/java/br/com/wallet/WalletApplication.java
@@ -2,7 +2,9 @@ package br.com.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class WalletApplication {
 
